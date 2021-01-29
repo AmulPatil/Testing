07482dd4cb27d494e72fcfb906fa64d813a32178
Amul <34025731+AmulPatil@users.noreply.github.com>
2021-01-29 19:18:47 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,20 @@
+def stain_recognition():
+  folders_1 = glob.glob('*.jpg')
+  folders_2 = glob.glob('*.jpeg')
+  single_list = [*folders_1,*folders_2]
+  for image_path in single_list[:1]:
+      im = Image.open(image_path)
+      im.convert('RGB').save("1.jpg","JPEG") #this converts png image as jpeg
+  img = cv2.imread('1.jpg', cv2.IMREAD_UNCHANGED)
+  cv2_imshow(img)
+  pred,pred_idx,probs = learn.predict(img)
+  #remove jpg file for next file
+  folders_1 = glob.glob('*.jpg')
+  folders_2 = glob.glob('*.jpeg')
+  filelist = [*folders_1,*folders_2]
+  for f in filelist:
+      os.remove(f)
+  print('------------------------------------')
+  print(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+  print('------------------------------------')
+  return 
